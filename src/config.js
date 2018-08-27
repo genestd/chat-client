25c4d00932d6628bda69fc66c65d232be31959f7
genestd <drwgenest@gmail.com>
2018-08-26 18:21:50 -0700

    Adds backend config for Amplify

---
+++ src/config.js
@@ -0,0 +1,8 @@
+const config = {
+  identityPoolId: 'us-west-2:13d610b0-64f7-49f4-aa68-3525f9779861',
+  region: 'us-west-2',
+  userPoolId: 'us-west-2_kwGNWMOvx',
+  appClient: '1pbm6ecgp46nc93rb2l58mpj1q',
+}
+
+export default config
