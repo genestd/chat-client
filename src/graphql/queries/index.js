94d729e1b3bec42b7c0d150b250e7240d6e2513b
genestd <drwgenest@gmail.com>
2018-08-27 07:55:27 -0700

    Adds getAllUsers query

--- src/graphql/queries/index.js
+++ src/graphql/queries/index.js
@@ -1,4 +1,12 @@
 const queries = {
+  GetAllUsers: `query GetAllUsers($limit: Int){
+    getAllUsers(limit: $limit) {
+      items {
+        username
+        name
+      }
+    }
+  }`,
   GetUserByEmail: `query GetUserByEmail($username: String!) {
     getUsers(username: $username) {
       username
