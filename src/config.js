c9d32928b00877e04bbc2bc28d289b0b998c1ee1
genestd <drwgenest@gmail.com>
2018-08-29 09:08:26 -0700

    Updates config for redeployment

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 const config = {
-  identityPoolId: 'us-west-2:13d610b0-64f7-49f4-aa68-3525f9779861',
+  identityPoolId: 'us-west-2:59e95598-c758-44c3-b818-3c7a5e03faef',
   region: 'us-west-2',
-  userPoolId: 'us-west-2_kwGNWMOvx',
-  appClient: '1pbm6ecgp46nc93rb2l58mpj1q',
-  appSyncEndpoint: 'https://7xk6bs3oljfslnelt4tuju3heu.appsync-api.us-west-2.amazonaws.com/graphql'
+  userPoolId: 'us-west-2_7ruS8Li0I',
+  appClient: '7na6j50u3u925q79khn8tpevll',
+  appSyncEndpoint: 'https://zxq25try3vdmxll7tgkyf4gtqe.appsync-api.us-west-2.amazonaws.com/graphql'
 }
 
 export default config
