6ca3c244e8badb968c980689f60a6e5651eecd54
genestd <drwgenest@gmail.com>
2018-08-28 23:34:17 -0700

    displays a conversation in the active conversation pane

--- src/components/chat/Conversation.js
+++ src/components/chat/Conversation.js
@@ -1,7 +1,17 @@
 import React from 'react'
 
 const Conversation = props => (
-  <div>Conversation</div>
+  <div id="conversation-container">
+    {
+      props.details.sort((a, b) => a.timestamp - b.timestamp)
+      .map(msg => {
+        const cssClass = msg.sender === props.user.email ? "-sender" : "-recipient"
+        return <div key={msg.messageId} className={`conversation__message${cssClass}`}>
+          <span className={`conversation__message${cssClass}-text`}>{msg.message}</span>
+        </div>
+      })
+    }
+  </div>
 )
 
 export default Conversation
