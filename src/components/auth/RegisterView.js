e2141bd3054b4d1e49f782becf7ef580b71226af
genestd <drwgenest@gmail.com>
2018-08-28 23:33:37 -0700

    Add some styling for the public pages

--- src/components/auth/RegisterView.js
+++ src/components/auth/RegisterView.js
@@ -2,26 +2,26 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const RegisterView = props => (
-  <div>
-    <h1>Register</h1>
-    <div>
+  <div className="login__container">
+    <div className="login__form">
+      <h1>Register</h1>
       <div>
         Already have an account? <Link to="/login">Sign in.</Link>
       </div>
       <div>
-        <input type="text" id="name" placeholder="name" onChange={props.handleInputChange} />
+        <input className="login__input" type="text" id="name" placeholder="name" onChange={props.handleInputChange} />
       </div>
       <div>
-        <input type="text" id="email" placeholder="email" onChange={props.handleInputChange} />
+        <input className="login__input" type="text" id="email" placeholder="email" onChange={props.handleInputChange} />
       </div>
       <div>
-        <input type="password" id="password" placeholder="password" onChange={props.handleInputChange} />
+        <input className="login__input" type="password" id="password" placeholder="password" onChange={props.handleInputChange} />
       </div>
       <div>
-        <input type="password" id="confirmPassword" placeholder="confirm password" onChange={props.handleInputChange} />
+        <input className="login__input" type="password" id="confirmPassword" placeholder="confirm password" onChange={props.handleInputChange} />
       </div>
-      <button onClick={props.handleRegister}>Register</button>
-      <div>
+      <button className="login__button" onClick={props.handleRegister}>Register</button>
+      <div className="login__text-error">
         { props.error && props.message}
       </div>
     </div>
