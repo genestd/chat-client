d5b76279bfe805e261cc830f65db23b0a3390a06
dgenest <dave.genest@popart.com>
2018-09-01 22:25:44 -0700

    Escape double quotes using JSONl.stringify

--- src/components/chat/Home.js
+++ src/components/chat/Home.js
@@ -58,13 +58,14 @@ class Home extends Component {
   // Create add message mutation on ENTER key
   handleKeyPress = async e => {
     if (e.key === 'Enter' || e.charCode ===13 ) {
+
       const result = await API.graphql(graphqlOperation(mutations.addMessage, {
         id: this.props.appState.activeConversation,
         messageId: md5(`${this.state.message}${Date.now()}`),
         timestamp: Math.floor(Date.now()/1000),
         sender: this.props.user.email,
         participants: this.props.appState.conversationDetails[0].participants,
-        message: this.state.message,
+        message: JSON.stringify(this.state.message).slice(1, -1),
       }))
       this.setState({message: ''})
     }
