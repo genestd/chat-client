6b7d8aa32739e5aee7f6cbf526aa7350415502cd
dgenest <dave.genest@popart.com>
2018-09-01 20:55:13 -0700

    Styling for the pending registration page

--- src/components/auth/Pending.js
+++ src/components/auth/Pending.js
@@ -2,8 +2,15 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Pending = props => (
-  <div>
-    Thank you for registering.  An email has been sent to your account.  Please click the link to verify, and then proceed to <Link to="/login">Sign in</Link>
+  <div className="login__container">
+    <div className="login__pending">
+        <h1>
+            Almost done...
+        </h1>
+        <div className="login__pending-body">
+            Thank you for registering.  An email has been sent to your account.  Please click the link to verify, and then proceed to <Link to="/login">Sign in</Link>
+        </div>
+    </div>
   </div>
 )
 
